b26ddc6b276175b8fa8af9c5ff882536801c0191
Fernando Antunes <f.antunes84@gmail.com>
2025-03-28 19:18:33 -0300

    ✨ Autenticação via JWT

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 install:
 	@poetry init
-	@poetry add scikit-learn pandas fastapi uvicorn
+	@poetry install
 run:
-	@poetry shell
 	@uvicorn api.main:app --reload --host 0.0.0.0 --port 8001
--- api/main.py
+++ api/main.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl='http://localhost:8000/api/token/')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+
+def verify_token(token: str = Depends(OAUTH2_SCHEME)):
+    """
+    Verifica se a requisição possui o token de autentificação.
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Token inválido ou expirado',
+            headers={'WWW-Authenticate': 'Bearer'}
+        ) from exc
+
 @app.get('/')
-def read_root():
+def read_root(payload: dict = Depends(verify_token)):
+    """
+    Exibe uma mensagem de boas vindas para o usuário autenticado.
+    """
+    print(payload)
     return {'message': 'Seja bem vindo ao Transaction Classifier API!'}
