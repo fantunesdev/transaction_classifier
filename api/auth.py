3b8471f5bceb3a1583b47e7dab08e47f33cbe757
Fernando Antunes <f.antunes84@gmail.com>
2025-03-28 19:51:01 -0300

    ♻️ Melhoria na divisão de responsabilidades

---
+++ api/auth.py
@@ -0,0 +1,27 @@
+import os
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN_URL = os.getenv('TOKEN_URL')
+OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl=TOKEN_URL)
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+
+def verify_token(token: str = Depends(OAUTH2_SCHEME)):
+    """
+    Verifica se a requisição possui o token de autentificação.
+    """
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Token inválido ou expirado',
+            headers={'WWW-Authenticate': 'Bearer'}
+        ) from exc
--- api/main.py
+++ api/main.py
@@ -1,31 +1,8 @@
-import os
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from dotenv import load_dotenv
+from fastapi import FastAPI, Depends
 
-load_dotenv()
+from api.auth import verify_token
 
 app = FastAPI()
-
-OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl='http://localhost:8000/api/token/')
-
-SECRET_KEY = os.getenv('SECRET_KEY')
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
-
-def verify_token(token: str = Depends(OAUTH2_SCHEME)):
-    """
-    Verifica se a requisição possui o token de autentificação.
-    """
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except JWTError as exc:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Token inválido ou expirado',
-            headers={'WWW-Authenticate': 'Bearer'}
-        ) from exc
 
 @app.get('/')
 def read_root(payload: dict = Depends(verify_token)):
